22c14be8dcf2e186f5848c5fbd171b813e597e78
siyavash-afeilyzadeh <siyavash.afeilyzadeh@gmail.com>
2026-06-26 22:13:31 +0330

    init commit

--- src/main/java/black/service/CurrencyConvertor.java
+++ src/main/java/black/service/CurrencyConvertor.java
@@ -1,5 +1,11 @@
 package black.service;
 
 public class CurrencyConvertor {
-
+   private final static double USD_IRT = 161500F;
+    public double toRial(double amount) {
+        return amount * USD_IRT;
+    }
+    public double toUSD(double amount){
+        return amount/USD_IRT
+    }
 }
